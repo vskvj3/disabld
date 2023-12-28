c1d2237716f77d5cc00820ed920d526738f28420
vskvj3 <92223768+vskvj3@users.noreply.github.com>
2023-12-28 22:33:01 +0530

    feat: read config file

---
+++ src/deamonize.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <signal.h>
+#include <getopt.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+
+/**
+ * \brief This function will daemonize this app
+ */
+static void daemonize(char *pid_file_name, int pid_fd)
+{
+	pid_t pid = 0;
+	int fd;
+
+	/* Fork off the parent process */
+	pid = fork();
+
+	/* An error occurred */
+	if (pid < 0) {
+		exit(EXIT_FAILURE);
+	}
+
+	/* Success: Let the parent terminate */
+	if (pid > 0) {
+		exit(EXIT_SUCCESS);
+	}
+
+	/* On success: The child process becomes session leader */
+	if (setsid() < 0) {
+		exit(EXIT_FAILURE);
+	}
+
+	/* Ignore signal sent from child to parent process */
+	signal(SIGCHLD, SIG_IGN);
+
+	/* Fork off for the second time*/
+	pid = fork();
+
+	/* An error occurred */
+	if (pid < 0) {
+		exit(EXIT_FAILURE);
+	}
+
+	/* Success: Let the parent terminate */
+	if (pid > 0) {
+		exit(EXIT_SUCCESS);
+	}
+
+	/* Set new file permissions */
+	umask(0);
+
+	/* Change the working directory to the root directory */
+	/* or another appropriated directory */
+	chdir("/");
+
+	/* Close all open file descriptors */
+	for (fd = sysconf(_SC_OPEN_MAX); fd > 0; fd--) {
+		close(fd);
+	}
+
+	/* Reopen stdin (fd = 0), stdout (fd = 1), stderr (fd = 2) */
+	stdin = fopen("/dev/null", "r");
+	stdout = fopen("/dev/null", "w+");
+	stderr = fopen("/dev/null", "w+");
+
+	/* Try to write PID of daemon to lockfile */
+	if (pid_file_name != NULL)
+	{
+		char str[256];
+		pid_fd = open(pid_file_name, O_RDWR|O_CREAT, 0640);
+		if (pid_fd < 0) {
+			/* Can't open lockfile */
+			exit(EXIT_FAILURE);
+		}
+		if (lockf(pid_fd, F_TLOCK, 0) < 0) {
+			/* Can't lock file */
+			exit(EXIT_FAILURE);
+		}
+		/* Get current PID */
+		sprintf(str, "%d\n", getpid());
+		/* Write PID to lockfile */
+		write(pid_fd, str, strlen(str));
+	}
+}
--- src/utils.c
+++ src/utils.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <dirent.h>
 #include <string.h>
+#include <syslog.h>
+#include <errno.h>
 
 void list_input_devices() {
     DIR *dir;
@@ -29,5 +31,42 @@ void list_input_devices() {
         }
     }
     closedir(dir);
+}
+
+/**
+ * \brief Read configuration from config file
+ */
+int read_conf_file(int reload, char *app_name, char *conf_file_name)
+{
+	FILE *conf_file = NULL;
+	int ret = -1, delay;
+
+	if (conf_file_name == NULL) return 0;
+
+	conf_file = fopen(conf_file_name, "r");
+
+	if (conf_file == NULL) {
+		syslog(LOG_ERR, "Can not open config file: %s, error: %s",
+				conf_file_name, strerror(errno));
+		return -1;
+	}
+
+	ret = fscanf(conf_file, "%d", &delay);
+
+	if (ret > 0) {
+		if (reload == 1) {
+			syslog(LOG_INFO, "Reloaded configuration file %s of %s",
+				conf_file_name,
+				app_name);
+		} else {
+			syslog(LOG_INFO, "Configuration of %s read from file %s",
+				app_name,
+				conf_file_name);
+		}
+	}
+
+	fclose(conf_file);
+
+	return ret;
 }
 
